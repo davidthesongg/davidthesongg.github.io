3f5b4ed0e1152d10adbb6dee6e895c14c6e88796
davidthesongg <127637785+davidthesongg@users.noreply.github.com>
2023-04-15 20:18:11 -0700

    Update self_portrait.js

--- self_portrait/self_portrait.js
+++ self_portrait/self_portrait.js
@@ -1,61 +1,141 @@
-function setup() 
-{
-  createCanvas(600, 600);
-  background(191, 179, 163);
-}
+<!DOCTYPE html>
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+	<head>
+    		<title>Self-Portrait with p5.js</title>
+		<link rel="icon" type="image/png" href="cute_favicon_cat.png">
+		<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+		<style>
+     			
+			body 
+			{
+				font-size: 25px;
+				background: linear-gradient(to bottom right, #9CDBB4, #9CD9DB, #9CA3DB, #9CA3DB, #DBC19C);
+			background-size: cover;
+			background-attachment: fixed;
+			}
 
-function draw() 
-{
-  //body ~ referenced from Olivia
-  fill(46, 43, 39);
-  rect(160, 473, 60, 270, 30, 20, 5, 5);
-  rect(380, 473, 60, 270, 20, 30, 5, 5);
-  rect(170, 470, 260, 270, 40, 40, 5, 5);
+			h1 
+			{
+				background-color: rgba(255, 255, 255, 0.5); 
+				padding: 10px;
+				text-align: center; 
+				font-size: 50px; 
+			}
+			h2 
+			{
+				background-color: rgba(255, 255, 255, 0.5); 
+				padding: 10px;
+				font-size: 35px; 
+			}
+			
+			.centered-image 
+			{
+        			display: block;
+       				margin: 0 auto;
+        			width: 38%;
+        			height: auto;
+			}
+
+			p 
+			{
+				background-color: rgba(255, 255, 255, 0.5); 
+				padding: 10px;
+				font-size: 30px; 
+				font-family: 'Abril Fatface';
+				font-family: cursive;
+			}
+
+			ul 
+			{
+				background-color: rgba(255, 255, 255, 0.5); 
+				padding: 10px;
+				text-align: center; 
+				font-size: 30px; 
+			}
+
+    		</style>
+
+	</head>
+
+	<body style="background-color:#A3C1AE;">
+		<h1> 
+			S E L F - P O R T R A I T
+		</h1>
+		<p>
+			Here is my self-portrait that I drew using p5.js. I used a series of arcs, rectangles, and ellipses to draw myself. <br><br>
+
+		</p>
+		
+		<img src="selfie.png" class="rounded centered-image" alt="selfie"><br>
+
+		<p>
+			function setup() <br>
+{<br>
+  createCanvas(600, 600);<br>
+  background(191, 179, 163);<br>
+}<br>
+<br>
+
+function draw() <br>
+{<br>
+  //body ~ referenced from Olivia<br>
+  fill(46, 43, 39);<br>
+  rect(160, 473, 60, 270, 30, 20, 5, 5);<br>
+  rect(380, 473, 60, 270, 20, 30, 5, 5);<br>
+  rect(170, 470, 260, 270, 40, 40, 5, 5);<br>
   
-  //neck
-  fill(255, 238, 200)
-  rect(275, 430, 50, 55, 20);
- 
-  //hair
-  noStroke()
-  fill(32, 19, 19);
-  ellipse(300, 260, 280, 300);
-  
-  //head
-  fill(255, 238, 219);
-  ellipse(300, 300, 250, 300);
-  
-  //nose
-  fill(250, 211, 145)
-  ellipse(300, 335, 25, 50);
-  
-  //mouth
-  fill(255, 102, 102);
-  arc(300, 380, 49, 35, 0, PI);
-  
-  //left eye
-  fill(255, 255, 255)
-  ellipse(250, 300, 50, 25);
-  fill(0, 0, 0)
-  ellipse(250, 300, 20, 20);
-  
-  //right eye
-  fill(255, 255, 255)
-  ellipse(350, 300, 50, 25);
-  fill(0, 0, 0)
-  ellipse(350, 300, 20, 20);
-  
-  //eyebrow
-  stroke(21, 19, 19);
-  strokeWeight(3);
-  noFill();
-  arc(250, 275, 45, 15, PI, 6, OPEN);
-  arc(350, 275, 45, 15, 6.5, 3, OPEN);
-  
-  //bangs
-  noStroke()
-  fill(32, 19, 19);
-  arc(350, 175, 100, 90, 0, PI + QUARTER_PI, CHORD);
-  arc(240, 140, 140, 148, 0, 0.75 * PI);
-}
+  //neck<br>
+  fill(255, 238, 200)<br>
+  rect(275, 430, 50, 55, 20);<br>
+ <br>
+  //hair<br>
+  noStroke()<br>
+  fill(32, 19, 19);<br>
+  ellipse(300, 260, 280, 300);<br>
+  <br>
+  //head<br>
+  fill(255, 238, 219);<br>
+  ellipse(300, 300, 250, 300);<br>
+  <br>
+  //nose<br>
+  fill(250, 211, 145)<br>
+  ellipse(300, 335, 25, 50);<br>
+  <br>
+  //mouth<br>
+  fill(255, 102, 102);<br>
+  arc(300, 380, 49, 35, 0, PI);<br>
+  <br>
+  //left eye<br>
+  fill(255, 255, 255)<br>
+  ellipse(250, 300, 50, 25);<br>
+  fill(0, 0, 0)<br>
+  ellipse(250, 300, 20, 20);<br>
+  <br>
+  //right eye<br>
+  fill(255, 255, 255)<br>
+  ellipse(350, 300, 50, 25);<br>
+  fill(0, 0, 0)<br>
+  ellipse(350, 300, 20, 20);<br>
+  <br>
+  //eyebrow<br>
+  stroke(21, 19, 19);<br>
+  strokeWeight(3);<br>
+  noFill();<br>
+  arc(250, 275, 45, 15, PI, 6, OPEN);<br>
+  arc(350, 275, 45, 15, 6.5, 3, OPEN);<br>
+  <br>
+  //bangs<br>
+  noStroke()<br>
+  fill(32, 19, 19);<br>
+  arc(350, 175, 100, 90, 0, PI + QUARTER_PI, CHORD);<br>
+  arc(240, 140, 140, 148, 0, 0.75 * PI);<br>
+}<br>
+		</p>
+
+		<ul>
+			<a href="index.html">p o r t f o l i o</a>
+		</ul>
+	</body>
+</html>
